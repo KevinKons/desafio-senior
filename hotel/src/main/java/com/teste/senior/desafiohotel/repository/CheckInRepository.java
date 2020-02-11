cdc105a14b84837db0487c1c50730530701365c5
KevinKons <kevin.kons@hotmail.com>
2020-02-11 19:12:45 -0300

    repositories

---
+++ hotel/src/main/java/com/teste/senior/desafiohotel/repository/CheckInRepository.java
@@ -0,0 +1,7 @@
+package com.teste.senior.desafiohotel.repository;
+
+import com.teste.senior.desafiohotel.model.CheckIn;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CheckInRepository extends CrudRepository<CheckIn, Long> {
+}
---
+++ hotel/src/main/java/com/teste/senior/desafiohotel/repository/HospedeRepository.java
@@ -0,0 +1,7 @@
+package com.teste.senior.desafiohotel.repository;
+
+import com.teste.senior.desafiohotel.model.Hospede;
+import org.springframework.data.repository.CrudRepository;
+
+public interface HospedeRepository extends CrudRepository<Hospede, Long> {
+}
